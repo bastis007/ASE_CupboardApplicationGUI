e6c93cc36ca97d998eab1a2b50456be49302e55a
bastis007 <schaefer.bastian@mail.de>
2022-03-19 12:22:38 +0100

    Neuer Verusch zum updaten der Kleidungsstücke, HTTP Fehler 405

--- src/CupboardApplicationGUI.java
+++ src/CupboardApplicationGUI.java
@@ -17,8 +17,8 @@ public class CupboardApplicationGUI {
         device.setFullScreenWindow(frame);
         frame.setTitle("Personal digital Cupboard");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        String data[][] = getCupboard();
-        String columns[] = getTableColumns();
+        String[][] data = getCupboard();
+        String[] columns = getTableColumns();
         JTable table = new JTable();
         DefaultTableModel model = new DefaultTableModel(data, columns);
         table.setModel(model);
@@ -32,9 +32,7 @@ public class CupboardApplicationGUI {
             catch (IOException | InterruptedException ex) {ex.printStackTrace();}
         });
         JButton addButton = new JButton("Add Item");
-        addButton.addActionListener(e -> {
-            addGarmentInputField();
-        });
+        addButton.addActionListener(e -> addGarmentInputField());
         JButton deleteButton = new JButton("Delete Item");
         deleteButton.addActionListener(e -> {
             int index = table.getSelectedRow();
@@ -60,8 +58,7 @@ public class CupboardApplicationGUI {
                 String size = (String) table.getModel().getValueAt(index, 2);
                 String colour = (String) table.getModel().getValueAt(index, 3);
                 System.out.println(id);
-                try {updateGarment(id, type, size, colour);}
-                catch (IOException ex) {ex.printStackTrace();}
+                updateGarmentInputField(id, type, size, colour);
             }
         });
         buttonPanel.add(retrieveButton);
@@ -72,86 +69,6 @@ public class CupboardApplicationGUI {
         panel.add(table, BorderLayout.CENTER);
         frame.add(panel);
         frame.setVisible(true);
-    }
-
-    private HttpURLConnection getHttpGETUrlConnection() throws IOException {
-        String url = "http://localhost:8080/garment/";
-        URL urlObj = new URL(url);
-        HttpURLConnection connection = (HttpURLConnection) urlObj.openConnection();
-        connection.setRequestMethod("GET");
-        return connection;
-    }
-
-    private HttpURLConnection getHttpPOSTURLConnection(String url, String requestMethod) throws IOException {
-        URL urlObj = new URL(url);
-        HttpURLConnection connection = (HttpURLConnection) urlObj.openConnection();
-        connection.setRequestMethod(requestMethod);
-        connection.setRequestProperty("Content-Type", "application/json; utf-8");
-        connection.setRequestProperty("Accept", "application/json");
-        connection.setDoOutput(true);
-        return connection;
-    }
-
-    private StringBuffer getInputStream(HttpURLConnection connection) throws IOException {
-        BufferedReader inputReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-        while ((inputLine = inputReader.readLine()) != null) {
-            response.append(inputLine);
-        }
-        inputReader.close();
-        return response;
-    }
-
-    private void httpResponse(HttpURLConnection connection) throws IOException {
-        int responseCode = connection.getResponseCode();
-        System.out.println("Response Code : " + responseCode);
-        if(responseCode == 201){
-            JOptionPane.showMessageDialog(null,"Successfully added new Garment");
-        }
-        if(responseCode == 204){
-            JOptionPane.showMessageDialog(null,"Successfully deleted Garment");
-        }
-        if(responseCode > 204){
-            JOptionPane.showMessageDialog(null,"Internal error. Please try again. If this error continues please contact the administrator", "Digital Cupboard - Error", JOptionPane.ERROR_MESSAGE);
-        }
-    }
-
-    private String[][] extractData(String[] table) {
-        int l = 0;
-        String[][] data = new String[table.length][4];
-        for (String s : table) {
-            String[] tab = s.split(",\"_links\":");
-            for (int j = 0; j < tab.length; j++) {
-                if (j % 2 != 0) {
-                    tab[j] = null;
-                }
-                if (j + 1 == tab.length) {
-                    tab[j] = null;
-                }
-            }
-            for (int j = 0; (j + 1) < tab.length; j++) {
-                if (tab[j] == null && j < tab.length) {
-                    tab[j] = tab[j + 1];
-                }
-                if (tab[j] == null) {
-                    tab[j] = tab[j - 1];
-                }
-                String[] t = tab[j].split(",");
-                for (int k = 0; k < t.length; k++) {
-                    t[k] = t[k].replace("\"type\":", "");
-                    t[k] = t[k].replace("\"size\":", "");
-                    t[k] = t[k].replace("\"colour\":", "");
-                    t[k] = t[k].replace("\"", "");
-                    t[k] = t[k].replace("\"", "");
-                    data[l][k] = t[k];
-                    if (k == 3) {
-                        l++;
-                    }
-                }
-            }
-        }
-        return data;
     }
 
     private void deleteGarment(String id) throws IOException {
@@ -166,26 +83,7 @@ public class CupboardApplicationGUI {
         httpResponse(connection);
     }
 
-    private void updateGarment(String id, String type, String size, String colour) throws IOException {
-        /*String url = "http://localhost:8080/garment/" + id + "/";
-        String requestMethod = "PUT";
-        HttpURLConnection connection = getHttpPOSTURLConnection(url, requestMethod);*/
-        String[] updatedGarment = updateGarmentInputField(id, type, size, colour);
-        size = updatedGarment[0];
-        colour = updatedGarment[1];
-        String body = "{\"id\":\"" + id + "\", \"type\":\"" + type + "\", \"size\":\"" + size + "\", \"colour\":\"" + colour + "\"}";
-        System.out.println(body);
-        /*deleteGarment(id);
-        addGarment(body);*/
-        /*try(OutputStream os = connection.getOutputStream()) {
-            byte[] input = body.getBytes(StandardCharsets.UTF_8);
-            os.write(input, 0, input.length);
-        }
-        httpResponse(connection);*/
-    }
-
-    private String[] updateGarmentInputField(String id, String type, String size, String colour) {
-        String[] updatedInput = {null, null};
+    private void updateGarmentInputField(String id, String type, String size, String colour) {
         JFrame jf = new JFrame ();
         jf.setSize (350, 300);
         BorderLayout borderLayout = new BorderLayout();
@@ -202,20 +100,6 @@ public class CupboardApplicationGUI {
         JLabel labelColour = new JLabel ("Enter Colour");
         JTextField fieldColour = new JTextField (10);
         JButton jb = new JButton ("Submit");
-        String[] newSize = {fieldSize.getText()};
-        String[] newColour = {fieldColour.getText()};
-        jb.addActionListener (e -> {
-
-            if(newSize[0].isEmpty() && newColour[0].isEmpty()){
-                JOptionPane.showMessageDialog(null,"Everything seems to be up to date", "Update Warning", JOptionPane.WARNING_MESSAGE);
-            }
-            else if(newColour[0].isEmpty()){
-                newColour[0] = colour;
-            }
-            else if(newSize[0].isEmpty()){
-                newSize[0] = size;
-            }
-        });
         panel.add(labelId);
         panel.add(fieldId);
         panel.add(labelType);
@@ -227,35 +111,36 @@ public class CupboardApplicationGUI {
         jf.add(panel, BorderLayout.CENTER);
         jf.add (jb, BorderLayout.SOUTH);
         jf.setVisible (true);
-        updatedInput = new String[]{newSize[0], newColour[0]};
-        return updatedInput;
+        jb.addActionListener (e -> {
+            String[] newSize = {fieldSize.getText()};
+            String[] newColour = {fieldColour.getText()};
+            if(newSize[0].isEmpty() && newColour[0].isEmpty()){
+                JOptionPane.showMessageDialog(null,"Everything seems to be up to date", "Update Warning", JOptionPane.WARNING_MESSAGE);
+            }
+            else if(newColour[0].isEmpty()){
+                newColour[0] = colour;
+            }
+            else if(newSize[0].isEmpty()){
+                newSize[0] = size;
+            }
+            try {
+                String body = "{\"id\":\"" + id + "\", \"type\":\"" + type + "\", \"size\":\"" + newSize[0] + "\", \"colour\":\"" + newColour[0] + "\"}";
+                updateGarment(body, id);
+            } catch (IOException ex) {ex.printStackTrace();}
+        });
     }
 
-    private void addGarment(String body) throws IOException {
-        String url = "http://localhost:8080/garment/";
-        String requestMethod = "POST";
+    private void updateGarment(String body, String id) throws IOException {
+        /*deleteGarment(id);
+        addGarment(body);*/
+        String url = "http://localhost:8080/garment/" + id + "/";
+        String requestMethod = "PUT";
         HttpURLConnection connection = getHttpPOSTURLConnection(url, requestMethod);
         try(OutputStream os = connection.getOutputStream()) {
             byte[] input = body.getBytes(StandardCharsets.UTF_8);
             os.write(input, 0, input.length);
         }
         httpResponse(connection);
-    }
-
-    private String[][] getCupboard() throws IOException, InterruptedException {
-        HttpURLConnection connection = getHttpGETUrlConnection();
-        int responseCode = connection.getResponseCode();
-        System.out.println("Response Code : " + responseCode);
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            StringBuffer response = getInputStream(connection);
-            String tableContent = response.toString();
-            String[] table = tableContent.split("\"id\":");
-            String[][] data = extractData(table);
-            httpResponse(connection);
-            return data;
-        }
-        httpResponse(connection);
-        return null;
     }
 
     private void addGarmentInputField() {
@@ -316,9 +201,118 @@ public class CupboardApplicationGUI {
         jf.setVisible (true);
     }
 
+    private void addGarment(String body) throws IOException {
+        String url = "http://localhost:8080/garment/";
+        String requestMethod = "POST";
+        HttpURLConnection connection = getHttpPOSTURLConnection(url, requestMethod);
+        try(OutputStream os = connection.getOutputStream()) {
+            byte[] input = body.getBytes(StandardCharsets.UTF_8);
+            os.write(input, 0, input.length);
+        }
+        httpResponse(connection);
+    }
+
     private String[] getTableColumns() {
-        String[] columns = {"ID", "Type", "Size", "Colour"};
-        return columns;
+        return new String[]{"ID", "Type", "Size", "Colour"};
+    }
+
+    private String[][] getCupboard() throws IOException, InterruptedException {
+        HttpURLConnection connection = getHttpGETUrlConnection();
+        int responseCode = connection.getResponseCode();
+        System.out.println("Response Code : " + responseCode);
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            StringBuffer response = getInputStream(connection);
+            String tableContent = response.toString();
+            String[] table = tableContent.split("\"id\":");
+            String[][] data = extractData(table);
+            httpResponse(connection);
+            return data;
+        }
+        httpResponse(connection);
+        return null;
+    }
+
+    private HttpURLConnection getHttpGETUrlConnection() throws IOException {
+        String url = "http://localhost:8080/garment/";
+        URL urlObj = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) urlObj.openConnection();
+        connection.setRequestMethod("GET");
+        return connection;
+    }
+
+    private HttpURLConnection getHttpPOSTURLConnection(String url, String requestMethod) throws IOException {
+        URL urlObj = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) urlObj.openConnection();
+        connection.setRequestMethod(requestMethod);
+        connection.setRequestProperty("Content-Type", "application/json; utf-8");
+        connection.setRequestProperty("Accept", "application/json");
+        connection.setDoOutput(true);
+        return connection;
+    }
+
+    private StringBuffer getInputStream(HttpURLConnection connection) throws IOException {
+        BufferedReader inputReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = inputReader.readLine()) != null) {
+            response.append(inputLine);
+        }
+        inputReader.close();
+        return response;
+    }
+
+    private void httpResponse(HttpURLConnection connection) throws IOException {
+        int responseCode = connection.getResponseCode();
+        System.out.println("Response Code : " + responseCode);
+        if(responseCode == 200){
+            //Do nothing, everything worked fine, but we don't need to tell the user
+        }
+        else if(responseCode == 201){
+            JOptionPane.showMessageDialog(null,"Successfully added new Garment");
+        }
+        if(responseCode == 204){
+            JOptionPane.showMessageDialog(null,"Successfully deleted Garment");
+        }
+        if(responseCode > 204){
+            JOptionPane.showMessageDialog(null,"Internal error. Please try again. If this error continues please contact the administrator", "Digital Cupboard - Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private String[][] extractData(String[] table) {
+        int l = 0;
+        String[][] data = new String[table.length][4];
+        for (String s : table) {
+            String[] tab = s.split(",\"_links\":");
+            for (int j = 0; j < tab.length; j++) {
+                if (j % 2 != 0) {
+                    tab[j] = null;
+                }
+                if (j + 1 == tab.length) {
+                    tab[j] = null;
+                }
+            }
+            for (int j = 0; (j + 1) < tab.length; j++) {
+                if (tab[j] == null && j < tab.length) {
+                    tab[j] = tab[j + 1];
+                }
+                if (tab[j] == null) {
+                    tab[j] = tab[j - 1];
+                }
+                String[] t = tab[j].split(",");
+                for (int k = 0; k < t.length; k++) {
+                    t[k] = t[k].replace("\"type\":", "");
+                    t[k] = t[k].replace("\"size\":", "");
+                    t[k] = t[k].replace("\"colour\":", "");
+                    t[k] = t[k].replace("\"", "");
+                    t[k] = t[k].replace("\"", "");
+                    data[l][k] = t[k];
+                    if (k == 3) {
+                        l++;
+                    }
+                }
+            }
+        }
+        return data;
     }
 
     /*private String[][] getTableData() {
